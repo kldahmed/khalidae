ee6dd3eee72887833f40e33a0435e92281d2597b
kldahmed <realxlaaad@gmail.com>
2026-03-31 14:51:03 +0000

    fix add

--- src/app/tools/excel-programmer/page.tsx
+++ src/app/tools/excel-programmer/page.tsx
@@ -1,5 +1,5 @@
 
-import { ExcelProgrammerShell } from "@/features/builder";
+import ExcelProgrammerShell from "@/features/builder/ExcelProgrammerShell";
 
 export default function ExcelProgrammerPage() {
   return <ExcelProgrammerShell />;
